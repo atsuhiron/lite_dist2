10df5ae9f2732887579d0c1b8fbbf472ab8172bd
Justica <atsuronreplaced@gmail.com>
2025-03-25 21:51:22 +0900

    Add test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # lite_dist2
 Simple distributed computing system
+
+## Developing
+### Requirement
+- uv >= 0.6.5
+
+### run pytest
+```shell
+uv run pytest --cov --cov-config=pyproject.toml
+```
---
+++ tests/test_common.py
@@ -0,0 +1,51 @@
+import pytest
+
+import lite_dist2.common as common
+
+
+@pytest.mark.parametrize(
+    ("hex_str", "expected"),
+    [
+        ("0", 0),
+        ("a", 10),
+        ("a54f", 42319)
+    ]
+)
+def test_hex2int(hex_str: str, expected: int) -> None:
+    assert common.hex2int(hex_str) == expected
+
+
+@pytest.mark.parametrize(
+    ("int_val", "expected"),
+    [
+        (0, "0"),
+        (10, "a"),
+        (42319, "a54f")
+    ]
+)
+def test_int2hex(int_val: int, expected: str) -> None:
+    assert common.int2hex(int_val) == expected
+
+
+@pytest.mark.parametrize(
+    ("hex_str", "expected"),
+    [
+        ("0x0.0p+0", 0.0),
+        ("-0x0.0p+0", -0.0),
+        ("0x1.999999999999ap-4", 0.1)
+    ]
+)
+def test_hex2float(hex_str: str, expected: float) -> None:
+    assert common.hex2float(hex_str) == expected
+
+
+@pytest.mark.parametrize(
+    ("float_val", "expected"),
+    [
+        (0.0, "0x0.0p+0"),
+        (-0.0, "-0x0.0p+0"),
+        (0.1, "0x1.999999999999ap-4")
+    ]
+)
+def test_float2hex(float_val: float, expected: str) -> None:
+    assert common.float2hex(float_val) == expected
