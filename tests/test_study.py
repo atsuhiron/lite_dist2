08295035d3b180e4afd600d69c8e334230f6731b
Justica <atsuronreplaced@gmail.com>
2025-04-27 17:06:19 +0900

    move trial_table_test

--- tests/test_study.py
+++ tests/test_study.py
@@ -1,7 +1,8 @@
 import pytest
 
-from lite_dist2.study import TrialTable, TrialTableModel
+from lite_dist2.study import Study, StudyModel, StudyStrategyModel, SuggestStrategyModel
 from lite_dist2.trial import Mapping, TrialModel, TrialStatus
+from lite_dist2.trial_table import TrialTableModel
 from lite_dist2.value_models.line_segment import LineSegmentModel
 from lite_dist2.value_models.point import ScalerValue
 from lite_dist2.value_models.space import ParameterAlignedSpaceModel
@@ -10,81 +11,161 @@ from lite_dist2.value_models.space import ParameterAlignedSpaceModel
 @pytest.mark.parametrize(
     "model",
     [
-        TrialTableModel(
-            trials=[],
-            aggregated_parameter_space=None,
-        ),
-        TrialTableModel(
-            trials=[
-                TrialModel(
-                    study_id="some_study",
-                    trial_status=TrialStatus.running,
-                    parameter_space=ParameterAlignedSpaceModel(
-                        type="aligned",
-                        axes=[
-                            LineSegmentModel(
-                                type="int",
-                                size=10,
-                                step=1,
-                                start="0x0",
-                                ambient_index="0x0",
-                                ambient_size="0x64",
-                            ),
-                        ],
-                        check_lower_filling=True,
-                    ),
-                    result_type="scaler",
-                    result_value_type="float",
-                ),
-            ],
-            aggregated_parameter_space=None,
-        ),
-        TrialTableModel(
-            trials=[
-                TrialModel(
-                    study_id="some_study",
-                    trial_status=TrialStatus.running,
-                    parameter_space=ParameterAlignedSpaceModel(
-                        type="aligned",
-                        axes=[
-                            LineSegmentModel(
-                                type="int",
-                                size=10,
-                                step=1,
-                                start="0x0",
-                                ambient_index="0x0",
-                                ambient_size="0x64",
-                            ),
-                        ],
-                        check_lower_filling=True,
-                    ),
-                    result_type="scaler",
-                    result_value_type="float",
-                    result=[
-                        Mapping(
-                            param=(
-                                ScalerValue(
-                                    type="scaler",
-                                    value_type="int",
-                                    value="0x1",
-                                    name="p1",
-                                ),
-                            ),
-                            result=ScalerValue(
-                                type="scaler",
-                                value_type="float",
-                                value="0x1.0000000000000p+1",
-                                name="p2",
-                            ),
+        pytest.param(
+            StudyModel(
+                study_id="01",
+                name="s1",
+                study_strategy=StudyStrategyModel(type="all_calculation", study_strategy_param=None),
+                suggest_strategy=SuggestStrategyModel(type="sequential"),
+                parameter_space=ParameterAlignedSpaceModel(
+                    type="aligned",
+                    axes=[
+                        LineSegmentModel(
+                            name="x",
+                            type="int",
+                            size=100,
+                            step=1,
+                            start="0x0",
+                            ambient_size="0x64",
+                            ambient_index="0x0",
+                        ),
+                        LineSegmentModel(
+                            name="y",
+                            type="int",
+                            size=100,
+                            step=1,
+                            start="0x0",
+                            ambient_size="0x64",
+                            ambient_index="0x0",
                         ),
                     ],
+                    check_lower_filling=True,
                 ),
-            ],
-            aggregated_parameter_space=None,
+                result_type="scaler",
+                result_value_type="int",
+                trial_table=TrialTableModel(
+                    trials=[
+                        TrialModel(
+                            study_id="01",
+                            trial_status=TrialStatus.done,
+                            parameter_space=ParameterAlignedSpaceModel(
+                                type="aligned",
+                                axes=[
+                                    LineSegmentModel(
+                                        name="x",
+                                        type="int",
+                                        size=1,
+                                        step=1,
+                                        start="0x0",
+                                        ambient_size="0x64",
+                                        ambient_index="0x0",
+                                    ),
+                                    LineSegmentModel(
+                                        name="y",
+                                        type="int",
+                                        size=2,
+                                        step=1,
+                                        start="0x0",
+                                        ambient_size="0x64",
+                                        ambient_index="0x0",
+                                    ),
+                                ],
+                                check_lower_filling=True,
+                            ),
+                            result_type="scaler",
+                            result_value_type="float",
+                            result=[
+                                Mapping(
+                                    param=(
+                                        ScalerValue(type="scaler", value_type="int", value="0x0", name="x"),
+                                        ScalerValue(type="scaler", value_type="int", value="0x0", name="y"),
+                                    ),
+                                    result=ScalerValue(
+                                        type="scaler",
+                                        value_type="float",
+                                        value="0x1.0000000000000p-1",
+                                    ),
+                                ),
+                                Mapping(
+                                    param=(
+                                        ScalerValue(type="scaler", value_type="int", value="0x0", name="x"),
+                                        ScalerValue(type="scaler", value_type="int", value="0x1", name="y"),
+                                    ),
+                                    result=ScalerValue(
+                                        type="scaler",
+                                        value_type="float",
+                                        value="0x1.0000000000000p-2",
+                                    ),
+                                ),
+                            ],
+                        ),
+                        TrialModel(
+                            study_id="01",
+                            trial_status=TrialStatus.running,
+                            parameter_space=ParameterAlignedSpaceModel(
+                                type="aligned",
+                                axes=[
+                                    LineSegmentModel(
+                                        name="x",
+                                        type="int",
+                                        size=1,
+                                        step=1,
+                                        start="0x0",
+                                        ambient_size="0x64",
+                                        ambient_index="0x0",
+                                    ),
+                                    LineSegmentModel(
+                                        name="y",
+                                        type="int",
+                                        size=2,
+                                        step=1,
+                                        start="0x2",
+                                        ambient_size="0x64",
+                                        ambient_index="0x2",
+                                    ),
+                                ],
+                                check_lower_filling=True,
+                            ),
+                            result_type="scaler",
+                            result_value_type="float",
+                        ),
+                    ],
+                    aggregated_parameter_space={
+                        -1: [],
+                        0: [
+                            ParameterAlignedSpaceModel(
+                                type="aligned",
+                                axes=[
+                                    LineSegmentModel(
+                                        name="x",
+                                        type="int",
+                                        size=1,
+                                        step=1,
+                                        start="0x0",
+                                        ambient_size="0x64",
+                                        ambient_index="0x0",
+                                    ),
+                                    LineSegmentModel(
+                                        name="y",
+                                        type="int",
+                                        size=2,
+                                        step=1,
+                                        start="0x0",
+                                        ambient_size="0x64",
+                                        ambient_index="0x0",
+                                    ),
+                                ],
+                                check_lower_filling=True,
+                            ),
+                        ],
+                    },
+                ),
+            ),
+            id="full_definition",
         ),
     ],
 )
-def test_trial_table_to_model_from_model(model: TrialTableModel) -> None:
-    table = TrialTable.from_model(model)
-    reconstructed_model = table.to_model()
-    assert model == reconstructed_model
+def test_study_to_model_from_model(model: StudyModel) -> None:
+    study = Study.from_model(model)
+    reconstructed = study.to_model()
+    assert model == reconstructed
--- tests/test_trial_table.py
+++ tests/test_trial_table.py
@@ -1,30 +1,35 @@
 import pytest
 
+from lite_dist2.study import TrialTableModel
+from lite_dist2.trial import Mapping, TrialModel, TrialStatus
 from lite_dist2.trial_table import TrialTable
-from lite_dist2.value_models.line_segment import ParameterRangeInt
-from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
+from lite_dist2.value_models.line_segment import LineSegmentModel, ParameterRangeInt
+from lite_dist2.value_models.point import ScalerValue
+from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace, ParameterAlignedSpaceModel
 
 
 @pytest.mark.parametrize(
     ("table", "total_num", "expected"),
     [
-        (  # Empty
+        pytest.param(  # Empty
             TrialTable(
                 trials=[],  # ここでは使わないので空
                 aggregated_parameter_space=None,
             ),
             100,
             FlattenSegment(0, None),
+            id="Empty",
         ),
-        (  # Empty
+        pytest.param(  # Empty
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={-1: [], 0: []},
             ),
             100,
             FlattenSegment(0, None),
+            id="Empty",
         ),
-        (  # Universal 1D
+        pytest.param(  # Universal 1D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -48,8 +53,9 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             10,
             FlattenSegment(10, 0),
+            id="Universal 1D",
         ),
-        (  # Infinite 1D
+        pytest.param(  # Infinite 1D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -73,8 +79,9 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             None,
             FlattenSegment(10, None),
+            id="Infinite 1D",
         ),
-        (  # Segmented 1D
+        pytest.param(  # Segmented 1D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -111,8 +118,9 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             10,
             FlattenSegment(10, 40),
+            id="Segmented 1D",
         ),
-        (  # Infinite segmented 1D
+        pytest.param(  # Infinite segmented 1D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -149,8 +157,9 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             None,
             FlattenSegment(10, 40),
+            id="Infinite segmented 1D",
         ),
-        (  # Universal 2D
+        pytest.param(  # Universal 2D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -185,8 +194,9 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             100,
             FlattenSegment(100, 0),
+            id="Universal 2D",
         ),
-        (  # Segmented 2D
+        pytest.param(  # Segmented 2D
             TrialTable(
                 trials=[],
                 aggregated_parameter_space={
@@ -245,6 +255,7 @@ from lite_dist2.value_models.space import FlattenSegment, ParameterAlignedSpace
             ),
             100,
             FlattenSegment(5, 5),
+            id="Segmented 2D",
         ),
     ],
 )
@@ -255,3 +266,337 @@ def test_trial_table_find_least_division(
 ) -> None:
     actual = table.find_least_division(total_num)
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("table", "expected"),
+    [
+        pytest.param(  # Empty
+            TrialTable(
+                trials=[],  # ここでは使わないので空
+                aggregated_parameter_space=None,
+            ),
+            0,
+            id="Empty_aps=None",
+        ),
+        pytest.param(  # Empty
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={-1: [], 0: []},
+            ),
+            0,
+            id="Empty",
+        ),
+        pytest.param(  # Universal 1D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                    0: [],
+                },
+            ),
+            10,
+            id="Universal 1D",
+        ),
+        pytest.param(  # Infinite 1D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [],
+                    0: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=None,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                },
+            ),
+            10,
+            id="Infinite 1D",
+        ),
+        pytest.param(  # Segmented 1D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [],
+                    0: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=100,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=50,
+                                    ambient_size=100,
+                                    start=50,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                },
+            ),
+            20,
+            id="Segmented 1D",
+        ),
+        pytest.param(  # Infinite segmented 1D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [],
+                    0: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=None,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    type="int",
+                                    size=10,
+                                    ambient_index=50,
+                                    ambient_size=None,
+                                    start=50,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                },
+            ),
+            20,
+            id="Infinite segmented 1D",
+        ),
+        pytest.param(  # Universal 2D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    name="x",
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                                ParameterRangeInt(
+                                    name="y",
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                    0: [],
+                    1: [],
+                },
+            ),
+            100,
+            id="Universal 2D",
+        ),
+        pytest.param(  # Segmented 2D
+            TrialTable(
+                trials=[],
+                aggregated_parameter_space={
+                    -1: [],
+                    0: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    name="x",
+                                    type="int",
+                                    size=1,
+                                    ambient_index=1,
+                                    ambient_size=10,
+                                    start=1,
+                                    step=1,
+                                ),
+                                ParameterRangeInt(
+                                    name="y",
+                                    type="int",
+                                    size=10,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                    1: [
+                        ParameterAlignedSpace(
+                            axes=[
+                                ParameterRangeInt(
+                                    name="x",
+                                    type="int",
+                                    size=1,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                                ParameterRangeInt(
+                                    name="y",
+                                    type="int",
+                                    size=5,
+                                    ambient_index=0,
+                                    ambient_size=10,
+                                    start=0,
+                                    step=1,
+                                ),
+                            ],
+                            check_lower_filling=True,
+                        ),
+                    ],
+                },
+            ),
+            15,
+            id="Segmented 2D",
+        ),
+    ],
+)
+def test_trial_table_count(
+    table: TrialTable,
+    expected: int,
+) -> None:
+    actual = table.count()
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        TrialTableModel(
+            trials=[],
+            aggregated_parameter_space=None,
+        ),
+        TrialTableModel(
+            trials=[
+                TrialModel(
+                    study_id="some_study",
+                    trial_status=TrialStatus.running,
+                    parameter_space=ParameterAlignedSpaceModel(
+                        type="aligned",
+                        axes=[
+                            LineSegmentModel(
+                                type="int",
+                                size=10,
+                                step=1,
+                                start="0x0",
+                                ambient_index="0x0",
+                                ambient_size="0x64",
+                            ),
+                        ],
+                        check_lower_filling=True,
+                    ),
+                    result_type="scaler",
+                    result_value_type="float",
+                ),
+            ],
+            aggregated_parameter_space=None,
+        ),
+        TrialTableModel(
+            trials=[
+                TrialModel(
+                    study_id="some_study",
+                    trial_status=TrialStatus.running,
+                    parameter_space=ParameterAlignedSpaceModel(
+                        type="aligned",
+                        axes=[
+                            LineSegmentModel(
+                                type="int",
+                                size=10,
+                                step=1,
+                                start="0x0",
+                                ambient_index="0x0",
+                                ambient_size="0x64",
+                            ),
+                        ],
+                        check_lower_filling=True,
+                    ),
+                    result_type="scaler",
+                    result_value_type="float",
+                    result=[
+                        Mapping(
+                            param=(
+                                ScalerValue(
+                                    type="scaler",
+                                    value_type="int",
+                                    value="0x1",
+                                    name="p1",
+                                ),
+                            ),
+                            result=ScalerValue(
+                                type="scaler",
+                                value_type="float",
+                                value="0x1.0000000000000p+1",
+                                name="p2",
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+            aggregated_parameter_space=None,
+        ),
+    ],
+)
+def test_trial_table_to_model_from_model(model: TrialTableModel) -> None:
+    table = TrialTable.from_model(model)
+    reconstructed_model = table.to_model()
+    assert model == reconstructed_model
